5903a0c0a6c9f2ac3ed32162ad9af986853274b1
Musa Ndhlovu <ndhlovumusa06@gmail.com>
2025-02-02 23:07:49 +0200

    alx_fe_javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMImplementaion", function(){
+
+});
+
+const SubmitButtion = document.getElementById("submitButton") 
+
+let showRandomQuote = ['spring', 'winter', 'summer', 'autum']
