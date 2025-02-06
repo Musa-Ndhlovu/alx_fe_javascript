ff7ddca6e370d7fcc89bcc855f6857dc2ccad449
Musa Ndhlovu <ndhlovumusa06@gmail.com>
2025-02-06 15:08:45 +0200

    alx_fe_javasrcipt

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -4,4 +4,4 @@ document.addEventListener("DOMImplementaion", function(){
 
 const SubmitButtion = document.getElementById("submitButton") 
 
-let showRandomQuote = ['spring', 'winter', 'summer', 'autum']
+let showRandomQuote = ["winter", "summer", "spring", "autum"]
