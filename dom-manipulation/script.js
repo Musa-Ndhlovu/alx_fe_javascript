b90ce885751d015e3f06f42c7ef5dbf63dab166b
Musa Ndhlovu <ndhlovumusa06@gmail.com>
2025-02-06 15:38:02 +0200

    alx_fe_javascript

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -5,3 +5,7 @@ document.addEventListener("DOMImplementaion", function(){
 const submitButton = document.getElementById("submitButton") 
 
 let  showRandomQuote = ["winter", "summer", "spring", "autum"]
+
+const form = documentElementById('createAddQuoteForm');
+
+form.addEventListener('click', function(submit))
