b2bc181202b97f929dce447c5f5059e0a4506292
Musa Ndhlovu <ndhlovumusa06@gmail.com>
2025-02-07 16:10:43 +0200

    alx_fe_javascript

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,10 +1,14 @@
+//JS Implementation
 document.addEventListener("DOMImplementaion", function(){
 
 });
 
 const submitButton = document.getElementById("submitButton") 
 
+
 let  showRandomQuote = ["winter", "summer", "spring", "autum"]
+
+console.log("showRandomQuote")
 
 const form = documentElementById('createAddQuoteForm');
 
